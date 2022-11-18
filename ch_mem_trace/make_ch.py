256702de2b124b80d883dab18aa0f19efa782ee1
7bvcxz@gmail.com <7bvcxz@gmail.com>
2022-11-18 13:09:48 +0900

    Added Channel memory trace

---
+++ ch_mem_trace/make_ch.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("g++ -o ch_mem_test ch_mem_trace.cpp")
+os.system("cp ch_mem_test ..")
---
+++ ch_mem_trace/make_ch0.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("g++ -o ch0_mem_test ch0_mem_trace.cpp")
+os.system("cp ch0_mem_test ..")
