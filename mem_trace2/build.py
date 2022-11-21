6be091938877c29dd232216e842bf529f0cb39d8
7bvcxz@gmail.com <7bvcxz@gmail.com>
2022-11-21 18:17:20 +0900

    Added T2_32B_thr_WR

--- mem_trace2/build.py
+++ mem_trace2/build.py
@@ -2,5 +2,5 @@ import os
 
 name = str(input())
 
-os.system("g++ -static -I /home/kkm0411/gem5-22.0.0/include -o ../"+name+" "+name+".cpp libm5.a")
+os.system("g++ -pthread -static -I /home/kkm0411/gem5-22.0.0/include -o ../"+name+" "+name+".cpp libm5.a")
 #os.system("mv "+name+" ..")
